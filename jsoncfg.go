b8a30ebc598534e3985ad160fb0c54b14d7c8f2e
MHall <mhall01248@gmail.com>
2025-01-22 15:17:56 +1030

    2025-01-22 15:17:56

---
+++ jsoncfg.go
@@ -0,0 +1,49 @@
+/*
+Read JSON config file into a struct.
+*/
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+)
+
+type Cfg struct {
+	Id		int
+	Name		string
+	Directions 	[]string
+	Countries	[]struct {
+		Name		string
+		Language	string
+		Greeting	string
+	}
+
+}
+
+var AppConfig Cfg
+
+func init() {
+    raw, err := ioutil.ReadFile("jsoncfg.json")
+    if err != nil {
+       fmt.Println("Error occured while reading config")
+       return
+    }
+    json.Unmarshal(raw, &AppConfig)
+}
+
+func main() {
+	fmt.Println("Id:", AppConfig.Id)
+	fmt.Println("Name:", AppConfig.Name)
+	fmt.Println("Directions:")
+	for _, direction := range AppConfig.Directions {
+		fmt.Println("-", direction)
+	}
+	fmt.Println("Countries:")
+	for _, country := range AppConfig.Countries {
+		fmt.Printf("- In %s they speak %s and say '%s!'.\n", country.Name, country.Language, country.Greeting)
+	}
+}
+
+
